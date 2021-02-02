5a39ce394b2d17297cf5bbb01b2905c22edf8773
zev <zevzhang8320@gmail.com>
2021-02-02 22:34:18 +0800

    assignment6-bvh

---
+++ assignment6/cmake-build-debug/CMakeFiles/RayTracing.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/zev/CLionProjects/graphics/games101-homework/assignment6/BVH.cpp" "/home/zev/CLionProjects/graphics/games101-homework/assignment6/cmake-build-debug/CMakeFiles/RayTracing.dir/BVH.cpp.o"
+  "/home/zev/CLionProjects/graphics/games101-homework/assignment6/Renderer.cpp" "/home/zev/CLionProjects/graphics/games101-homework/assignment6/cmake-build-debug/CMakeFiles/RayTracing.dir/Renderer.cpp.o"
+  "/home/zev/CLionProjects/graphics/games101-homework/assignment6/Scene.cpp" "/home/zev/CLionProjects/graphics/games101-homework/assignment6/cmake-build-debug/CMakeFiles/RayTracing.dir/Scene.cpp.o"
+  "/home/zev/CLionProjects/graphics/games101-homework/assignment6/Vector.cpp" "/home/zev/CLionProjects/graphics/games101-homework/assignment6/cmake-build-debug/CMakeFiles/RayTracing.dir/Vector.cpp.o"
+  "/home/zev/CLionProjects/graphics/games101-homework/assignment6/main.cpp" "/home/zev/CLionProjects/graphics/games101-homework/assignment6/cmake-build-debug/CMakeFiles/RayTracing.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
